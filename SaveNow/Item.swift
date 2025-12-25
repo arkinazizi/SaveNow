d0ff490797c041deb8c40dc379c3544cf0662f6d
Arkin Azizi <sajad.azizi90@gmail.com>
2025-12-25 12:55:18 +0300

    Initial Commit

---
+++ SaveNow/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  SaveNow
+//
+//  Created by Arkin Azizi on 25.12.2025.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ SaveNowTests/SaveNowTests.swift
@@ -0,0 +1,17 @@
+//
+//  SaveNowTests.swift
+//  SaveNowTests
+//
+//  Created by Arkin Azizi on 25.12.2025.
+//
+
+import Testing
+@testable import SaveNow
+
+struct SaveNowTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
